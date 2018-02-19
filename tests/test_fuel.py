f794c8c6a48e6f9017a7b28c25953b43bebcbea6
David Mougeolle <david.mougeolle@moodule.net>
2018-02-19 14:29:11 +0100

    basic tests on mpg conversions 	new file:   tests/test_fuel.py 	modified:   tests/test_wild.py

---
+++ tests/test_fuel.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for the fuel consumption tools."""
+
+from __future__ import absolute_import, division, print_function
+
+from numpy import isclose
+import pytest
+
+from wild import _fuel
+
+def test_conversions_mpg_to_lpkm():
+    assert _fuel.us_mpg_to_lpkm(0.0) == 0.0
+    assert _fuel.imp_mpg_to_lpkm(0.0) == 0.0
+
+    assert isclose(_fuel.us_mpg_to_lpkm(1.0), 235.215, rtol=1e-3)
+    assert isclose(_fuel.imp_mpg_to_lpkm(1.0), 282.481, rtol=1e-3)
+
+def test_conversions_lpkm_to_mpg():
+    assert _fuel.lpkm_to_us_mpg(1.0) == 0.0
+    assert _fuel.lpkm_to_imp_mpg(1.0) == 0.0
+
+    assert isclose(_fuel.lpkm_to_us_mpg(1.0), 235.215, rtol=1e-3)
+    assert isclose(_fuel.lpkm_to_imp_mpg(1.0), 282.481, rtol=1e-3)
