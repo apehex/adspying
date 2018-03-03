3271f71ff3ea9a7daecaba81efcd1df8b72e0768
David Mougeolle <david.mougeolle@moodule.net>
2018-03-03 17:43:51 +0100

    separated the package tools from the scripts using them. 	modified:   wild/_fuel.py 	modified:   wild/_scraping.py 	modified:   wild/_wrangling.py

--- wild/_wrangling.py
+++ wild/_wrangling.py
@@ -1,82 +1,18 @@
 from __future__ import absolute_import, division, print_function
 
-import numpy as np
-import pandas as pd
+"""Data cleaning tools."""
 
-###############################################################################
-# MERGED DF HEADERS
-###############################################################################
+#####################################################################
+# ENCODING & FORMAT
+#####################################################################
 
-MODEL_HEADERS = [
-'manufacturer',
-'model',
-'variant',
-'type',
-'year_min',
-'year_max']
+def convert_unicode_to_lower_string(unicode_bytes):
+    """Convert a unicode byte object to a standard string.
 
-SIZE_HEADERS = [
-'mass',
-'track_width',
-'wheel_base']
+    Args:
+        unicode_bytes (unicode): text, as a byte string.
 
-PRICE_HEADERS = [
-'price_new']
-
-ENGINE_HEADERS = [
-'fuel_type',
-'fuel_mode',
-'engine_capacity']
-        
-CONSUMPTION_HEADERS = [
-'fuel_consumption',
-'electricity_consumption']
-
-EMISSION_HEADERS = [
-'co2_emissions',
-'nox_emissions', 'mass']
-
-RATING_HEADERS = [
-'consumption_rating',
-'emission_rating',
-'price_rating',
-'size_rating']
-
-
-###############################################################################
-# READING
-###############################################################################
-
-DF = pd.read_csv('../data/raw/van-data.csv')
-
-###############################################################################
-# SWAPING MPG / L/100km
-###############################################################################
-
-df = pd.read_csv('van-data.csv')
-erroneous_lpkm = df[df['Combined Litres'] > 30.0][['Urban Litres', 'Extra Urban Litres', 'Combined Litres']]
-erroneous_mpg = df[df['Combined Litres'] > 30.0][['Urban MPG', 'Extra Urban MPG', 'Combined MPG']]
-
-df.loc[df['Combined Litres'] > 30.0][['Urban Litres', 'Extra Urban Litres', 'Combined Litres']] = erroneous_mpg
-df.loc[df['Combined Litres'] > 30.0][['Urban MPG', 'Extra Urban MPG', 'Combined MPG']] = erroneous_lpkm
-
-###############################################################################
-# REMOVING OUTLIERS
-###############################################################################
-
-###############################################################################
-# DEDUPING
-###############################################################################
-
-###############################################################################
-# NORMALISING
-###############################################################################
-
-# Create a minimum and maximum processor object
-min_max_scaler = preprocessing.MinMaxScaler()
-
-# Create an object to transform the data to fit minmax processor
-x_scaled = min_max_scaler.fit_transform(x)
-
-# Run the normalizer on the dataframe
-df_normalized = pd.DataFrame(x_scaled)
+    Returns:
+        str: text in lower case, as a standard python str type.
+    """
+    return unicode_bytes.encode('utf-8', 'ignore').lower()
