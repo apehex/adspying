d1e832576ef0ff73317cddb43a369e40a533e86b
apehex <apehex@protonmail.com>
2018-03-03 17:43:51 +0100

    separated the package tools from the scripts using them. 	modified:   wild/_fuel.py 	modified:   wild/_scraping.py 	modified:   wild/_wrangling.py

--- wild/_fuel.py
+++ wild/_fuel.py
@@ -5,9 +5,9 @@
 
 from __future__ import absolute_import, division, print_function
 
-################################################################################
+#####################################################################
 # CONSTANTS
-################################################################################
+#####################################################################
 
 US_GALLON = 3.785412	# l
 IMP_GALLON = 4.54609	# l
@@ -17,78 +17,70 @@ PETROL_COST = 1.56  # €/l
 DIESEL_COST = 1.52  # €/l
 ELECTRICITY_COST = 0.15	# €/kWh
 
-###############################################################################
+#####################################################################
 # FUEL EFFICIENCY
-###############################################################################
+#####################################################################
 
-def us_mpg_to_lpkm(mpg):
-	"""Convert US mpg to l/100km fuel consumption.
+def convert_us_mpg_to_lpkm(mpg):
+    """Convert US mpg to l/100km fuel consumption.
 
-	Args:
-		mpg (float): the fuel consumption in miles per US gallon.
+    Args:
+        mpg (float): the fuel consumption in miles per US gallon.
 
-	Returns:
-		float: the fuel consumption in liters per 100 kilometers.
-	"""
-	liters_per_hundred_kilometers = 100.0 * US_GALLON / MILE
-	if mpg > 0.0:
-		liters_per_hundred_kilometers = liters_per_hundred_kilometers / mpg
-	else:
-		liters_per_hundred_kilometers = 0.0
-	return liters_per_hundred_kilometers
+    Returns:
+        float: the fuel consumption in liters per 100 kilometers.
+    """
+    liters_per_hundred_kilometers = 100.0 * US_GALLON / MILE
+    if mpg > 0.0:
+        liters_per_hundred_kilometers = liters_per_hundred_kilometers / mpg
+    else:
+        liters_per_hundred_kilometers = 0.0
+    return liters_per_hundred_kilometers
 
-def imp_mpg_to_lpkm(mpg):
-	"""Convert Imp mpg to l/100km fuel consumption.
+def convert_imp_mpg_to_lpkm(mpg):
+    """Convert Imp mpg to l/100km fuel consumption.
 
-	Args:
-		mpg (float): the fuel consumption in miles per Imp gallon.
+    Args:
+        mpg (float): the fuel consumption in miles per Imp gallon.
 
-	Returns:
-		float: the fuel consumption in liters per 100 kilometers.
-	"""
-	liters_per_hundred_kilometers = 100.0 * IMP_GALLON / MILE
-	if mpg > 0.0:
-		liters_per_hundred_kilometers = liters_per_hundred_kilometers / mpg
-	else:
-		liters_per_hundred_kilometers = 0.0
-	return liters_per_hundred_kilometers
-	
+    Returns:
+        float: the fuel consumption in liters per 100 kilometers.
+    """
+    liters_per_hundred_kilometers = 100.0 * IMP_GALLON / MILE
+    if mpg > 0.0:
+        liters_per_hundred_kilometers = liters_per_hundred_kilometers / mpg
+    else:
+        liters_per_hundred_kilometers = 0.0
+    return liters_per_hundred_kilometers
+    
 
-def lpkm_to_us_mpg(lpkm):
-	"""Convert l/100km to US mpg fuel consumption.
+def convert_lpkm_to_us_mpg(lpkm):
+    """Convert l/100km to US mpg fuel consumption.
 
-	Args:
-		lpkm (float): the fuel consumption in liters per 100 kilometers.
+    Args:
+        lpkm (float): the fuel consumption in liters per 100 kilometers.
 
-	Returns:
-		float: the fuel consumption in miles per US gallon.
-	"""
-	return us_mpg_to_lpkm(lpkm)
+    Returns:
+        float: the fuel consumption in miles per US gallon.
+    """
+    return us_mpg_to_lpkm(lpkm)
 
-def lpkm_to_imp_mpg(lpkm):
-	"""Convert l/100km to Imp mpg fuel consumption.
+def convert_lpkm_to_imp_mpg(lpkm):
+    """Convert l/100km to Imp mpg fuel consumption.
 
-	Args:
-		lpkm (float): the fuel consumption in liters per 100 kilometers.
+    Args:
+        lpkm (float): the fuel consumption in liters per 100 kilometers.
 
-	Returns:
-		float: the fuel consumption in miles per Imp gallon.
-	"""
-	return imp_mpg_to_lpkm(lpkm)
+    Returns:
+        float: the fuel consumption in miles per Imp gallon.
+    """
+    return imp_mpg_to_lpkm(lpkm)
 
-###############################################################################
-# ESTIMATION
-###############################################################################
+#####################################################################
+# CONSUMPTION
+#####################################################################
 
-def estimate_consumption_to_emission_regression_factor():
-	import matplotlib.pyplot as plt
-	import pandas as pd
-
-	df = pd.read_csv('van-data.csv')
-
-	pass
-
-def estimate_consumption_from_emission(co2_emission, fuel_type='diesel'):
+def estimate_consumption_from_emission(co2_emission, fuel_type='diesel', load_weight=0.0):
     """Estimate the fuel consumption according to a given rate of CO2 emission.
 
     Args:
@@ -99,9 +91,24 @@ def estimate_consumption_from_emission(co2_emission, fuel_type='diesel'):
         float: the estimated fuel consumption, in l/100km.
     """
     if fuel_type == 'diesel':
-    	co2_mass_produced_by_fuel_combustion = 2640.0
+        rate = 0.03690051
     elif fuel_type == 'lpg':
-    	co2_mass_produced_by_fuel_combustion = 1665.0
+        rate = 0.0601
     else:
-		co2_mass_produced_by_fuel_combustion = 2392.0
-	return 100.0 * co2_emission / co2_mass_produced_by_fuel_combustion
+        rate = 0.04020645
+    return rate * co2_emission
+
+#####################################################################
+# POWER
+#####################################################################
+
+def convert_kw_to_hp(power_kw):
+    """Convert power from kilo Watts (kW) to horse power (HP).
+
+    Args:
+        power_kw (float): power, in kilo Watts.
+
+    Returns:
+        float: power, in horse power.
+    """
+    return 1.34102 * power_kw
--- wild/_scraping.py
+++ wild/_scraping.py
@@ -1,9 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Collect ads from Leboncoin."""
+
 from lxml import html
 import requests
+import urllib
 
-LEBONCOIN_URL_TEMPLATE = 'https://www.leboncoin.fr/utilitaires/offres/?o=1&pe=11&rs=2010'
+LEBONCOIN_BASE_URL = 'https://www.leboncoin.fr/utilitaires/offres/?'
 
-PAGE = requests.get(LEBONCOIN_URL_TEMPLATE)
+LEBONCOIN_URL_ARGS = {
+    'result_page_number': 'o',
+    'min_year': 'rs',
+    'max_year': 're',
+    'min_price': 'ps',
+    'max_price': 'pe',
+    'min_mileage': 'ms',
+    'max_mileage': 'me',
+    'fuel_type': 'fu'}
+
+LEBONCOIN_PRICE_DICT = {
+        0: 0,
+        1: 500,
+        2: 1000,
+        3: 2000,
+        4: 3000,
+        5: 4000,
+        6: 5000,
+        7: 6000,
+        8: 7000,
+        9: 8000,
+        10: 9000,
+        11: 10000,
+        12: 15000,
+        13: 20000,
+        14: 30000,
+        15: -1}
+
+LEBONCOIN_FUEL_DICT = {
+        'petrol': 1,
+        'diesel': 2,
+        'lpg': 3,
+        'electric': 4,
+        'hybrid': 5}
+
+
+
+PAGE = requests.get(LEBONCOIN_BASE_URL)
 TREE = html.fromstring(PAGE.content)
 
 #This will create a list of buyers:
