da39749bebad61f81a46bac6768cae19a1ce70a0
apehex <apehex@protonmail.com>
2019-11-09 20:23:47 +0100

    Add xpath to the attributes specific to shoe ads

--- wild/spiders/leboncoin.py
+++ wild/spiders/leboncoin.py
@@ -144,6 +144,20 @@ ITEM_AD_ATTRIBUTE_XPATH = {
         'div/div/div/div'
         + '/span[contains(@class, "content-CxPmi")]/text()'),}
 
+SHOE_AD_ATTRIBUTE_XPATH = {
+    'category': (
+        '//div[contains(@data-qa-id, "criteria_item_shoe_category")]'
+        + '/div/div[2]/text()'),
+    'color': (
+        '//div[contains(@data-qa-id, "criteria_item_clothing_color")]'
+        + '/div/div[2]/text()'),
+    'size': (
+        '//div[contains(@data-qa-id, "criteria_item_shoe_size")]'
+        + '/div/div[2]/text()'),
+    'type': (
+        '//div[contains(@data-qa-id, "criteria_item_shoe_type")]'
+        + '/div/div[2]/text()'),}
+
 #####################################################################
 # STRIP IRRELEVANT CHARS
 #####################################################################
