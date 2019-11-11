2058e80218b17e154b89d141d26ec240bd196ca7
David Mougeolle <moodule@protonmail.com>
2019-11-11 19:02:25 +0100

    Customize leboncoin scraping for real-estatesearches

--- wild/items.py
+++ wild/items.py
@@ -292,16 +292,45 @@ class VehiculeAdLoader(SecondHandAdLoader):
 class RealEstateAd(SecondHandAd):
     """
     """
-    # Generic
-    location = Field()
-
     # Specifications
+    category = Field()
     rooms = Field()
     floors = Field()
     indoor_area = Field()
     outdoor_area = Field()
     energy_grade = Field()
+    ghg_grade = Field()
 
     # Computed
     time_to_work = Field()
     buildable = Field()
+
+class RealEstateAdLoader(SecondHandAdLoader):
+    """
+    """
+    category_in = Join()
+    category_out = Identity()
+
+    rooms_in = Join()
+    rooms_out = Identity()
+
+    floors_in = Join()
+    floors_out = Identity()
+
+    indoor_area_in = Join()
+    indoor_area_out = Identity()
+
+    outdoor_area_in = Join()
+    outdoor_area_out = Identity()
+
+    energy_grade_in = Join()
+    energy_grade_out = Identity()
+
+    ghg_grade_in = Join()
+    ghg_grade_out = Identity()
+
+    time_to_work_in = Join()
+    time_to_work_out = Identity()
+
+    buildable_in = Join()
+    buildable_out = Identity()
