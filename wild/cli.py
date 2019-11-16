3b295da3bbe62ac5d6f4e5c81d28696fc4173463
apehex <apehex@protonmail.com>
2019-11-16 19:14:02 +0100

    Model the translation function: signature & doc

--- wild/cli.py
+++ wild/cli.py
@@ -36,7 +36,29 @@ def remove_special_characters(
         value)
 
 #####################################################################
-# CONTENT
+# TRANSLATE TEXT VALUES INTO URL ARGS
+#####################################################################
+
+@checks
+def translate_into_referential(
+        value: str,
+        referential: iterable) -> str:
+    """
+    Translate a random string into an item from a given referential.
+    In particular this is meant to translate user input into url argument
+    meaningful for the target website.
+
+    The referential itself can either be:
+    - a list
+    - a dictionary
+
+    The input value is matched with the closest item in the referential. 
+    The distance used depends on the kind of data compared.
+    """
+    return value
+
+#####################################################################
+# CLI
 #####################################################################
 
 @click.command()
