9acf2611ebd1fb54f97cfeb9e109ed375050e8e2
apehex <apehex@protonmail.com>
2019-11-16 19:12:51 +0100

    Add utility function to clean cli args

--- wild/cli.py
+++ wild/cli.py
@@ -10,7 +10,30 @@ Console script.
 
 from __future__ import absolute_import, division, print_function
 
+import re
+
 import click
+
+from typical import (
+    anything,
+    checks,
+    iterable,
+    numeric)
+
+#####################################################################
+# ARG CLEANING & STANDARDIZING
+#####################################################################
+
+@checks
+def remove_special_characters(
+        value: str) -> str:
+    """
+    Replace all the special characters in the argument by '_'. 
+    """
+    return re.sub(
+        '\W+',
+        '_',
+        value)
 
 #####################################################################
 # CONTENT
