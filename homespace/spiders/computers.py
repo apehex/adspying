da8ed7ae259c6383096e97e4b14ca3abace6de9c
apehex <apehex@protonmail.com>
2019-11-17 14:24:25 +0100

    Add a spider to scrape computer hardware

---
+++ homespace/spiders/computers.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+"""
+=========
+Computers
+=========
+
+Search for computers in ad listings:
+- desktop computers
+- laptops
+- hardware & parts
+"""
+
+from __future__ import division, print_function, absolute_import
+
+from homespace.items import ComputerAd, ComputerAdLoader
+from homespace.spiders._leboncoin import LeboncoinSpider
+
+#####################################################################
+# SEARCH ARG VALUES
+#####################################################################
+
+PRICE_VALUES = (
+    list(range(0, 50, 10))
+    + list(range(50, 100, 25))
+    + list(range(100, 550, 100)))
+
+#####################################################################
+# SPIDER
+#####################################################################
+
+class ComputersSpider(LeboncoinSpider):
+    name = 'computers'
+
+    def __init__(self, *args, **kwargs):
+        """
+        """
+        super(ComputersSpider, self).__init__(*args, **kwargs)
+
+        # forge a url to query leboncoin
+        self._search_args = {
+            'category': '15',
+            'locations': '',
+            'page': '1',
+            'price': '',
+            'shippable': '1',
+            'text': ''}
+
+        # scrape the resulting listing
+        self._ad_specific_attributes_xpath = {}
+
+        # classes to store, clean and export the data
+        self._item_class = ComputerAd
+        self._loader_class = ComputerAdLoader
