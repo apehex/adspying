19e7f183d19449658613ef05341413a6a42b9f72
apehex <apehex@protonmail.com>
2019-11-17 19:42:44 +0100

    Correct the meta informations, it was copied from another project

--- homespace/__init__.py
+++ homespace/__init__.py
@@ -14,7 +14,7 @@ __author__ = 'apehex'
 __email__ = 'apehex@protonmail.com'
 __version__ = '0.1.0'
 __title__ = 'homespace'
-__description__ = 'Generate blueprints to upcycle old items into art.'
+__description__ = 'A toolbox to scrape and visualize second hand ads.'
 __url__ = 'https://github/apehex/homespace'
 __license__ = 'MIT license'
 
