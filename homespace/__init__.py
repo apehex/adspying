e5256115660accc53fef3d0adf8d79d73e8a27ef
apehex <apehex@protonmail.com>
2019-11-17 12:32:14 +0100

    Change the meta: version, name, contact, etc

---
+++ homespace/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+=========
+Homespace
+=========
+
+A toolbox to scrape and visualize second hand ads.
+"""
+
+from __future__ import division, print_function, absolute_import
+
+__author__ = 'apehex'
+__email__ = 'apehex@protonmail.com'
+__version__ = '0.1.0'
+__title__ = 'homespace'
+__description__ = 'Generate blueprints to upcycle old items into art.'
+__url__ = 'https://github/apehex/homespace'
+__license__ = 'MIT license'
+
+__all__ = []
