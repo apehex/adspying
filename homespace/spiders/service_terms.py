653411a35a87cdfc34eb0bb68ea5e834d548e2d7
David Mougeolle <moodule@protonmail.com>
2019-11-24 12:43:01 +0100

    Select only the text of the service terms and filter out menus etc

--- homespace/spiders/service_terms.py
+++ homespace/spiders/service_terms.py
@@ -43,52 +43,73 @@ class ServiceTermsSpider(LegalDocumentsSpider):
         self.providers = {
             'auvieuxcampeur': {
                 'url': 'https://www.auvieuxcampeur.fr/mention-legale',
-                'selector': ''},
+                'selector': '//*[@id="maincontent"]'},
             'bluely': {
                 'url': 'https://www.bluely.eu/en/legal-notices',
-                'selector': ''},
+                'selector': (
+                    '//*[@id="main-content"]/main/div/div/div'
+                    + '/div[contains(@class, "content-base")]')},
             'clickworker': {
                 'url': 'https://workplace.clickworker.com/en/agreements/10123?_ga=2.234456860.300522425.1531068679-588812912.1531068679',
-                'selector': ''},
+                'selector': '//*[@id="agreements"]'},
             'github': {
                 'url': 'https://help.github.com/articles/github-terms-of-service/',
-                'selector': ''},
+                'selector': '//main/main/article'},
             'google': {
-                'url': 'https://policies.google.com/terms?hl=en&gl=zz',
-                'selector': ''},
+                'url': 'https://policies.google.com/terms',
+                'selector': (
+                    '//*[@id="yDmH0d"]/c-wiz/div/div/div[2]/div[3]/c-wiz/div'
+                    +' /div[contains(@class, "nrAB0c")]')},
             'homebox': {
                 'url': 'https://www.homebox.fr/mentions-legales',
-                'selector': ''},
+                'selector': (
+                    '/html/body/div[4]/div/div[2]/section'
+                    + '/div[contains(@class, "region"])')},
             'hardloop': {
                 'url': 'https://www.hardloop.fr/infos/cgu',
-                'selector': ''},
+                'selector': (
+                    '//*[@id="root"]/div'
+                    + '/div[contains(@class, "mainContent")]'
+                    + '/div[contains(@class, "InfoShowBannerWrapper")]'
+                    + '/div/div/div')},
             'kaggle': {
                 'url': 'https://www.kaggle.com/terms',
-                'selector': ''},
+                'selector': (
+                    '/html/body/main/div'
+                    + '/div[contains(@class, "main-content")]/div/div/div')},
             'medium': {
                 'url': 'https://medium.com/policy/medium-terms-of-service-9db0094a1e0f',
-                'selector': ''},
+                'selector': '//*[@id="root"]/div/article/div/section'},
             'microsoft': {
                 'url': 'https://www.microsoft.com/en/servicesagreement/',
-                'selector': ''},
+                'selector': '//div[contains(@class, "div_content")]'},
             'pivotal': {
                 'url': 'https://pivotal.io/svcs-terms',
-                'selector': ''},
+                'selector': (
+                    '//*[@id="viewport"]/div'
+                    + '/div[contains(@class, "content")]')},
             'pornhub': {
-                'url': 'https://fr.pornhub.com/information',
-                'selector': ''},
+                'url': 'https://fr.pornhub.com/information#terms',
+                'selector': '//*[@id="information_terms"]'},
             'redditinc': {
-                'url': 'https://www.redditinc.com/policies/content-policy',
-                'selector': ''},
+                'url': 'https://www.redditinc.com/policies/user-agreement',
+                'selector': '//*[@id="content"]'},
             'slack': {
-                'url': 'https://slack.com/intl/fr-fr/policy-archives/terms-of-service-user/2016-11-17',
-                'selector': ''},
+                'url': 'https://slack.com/intl/en-fr/terms-of-service',
+                'selector': (
+                    '//*[@id="main"]/div/div/div'
+                    + '/div[contains(@class, "legal-content")]')},
             'stackoverflow': {
                 'url': 'https://stackoverflow.com/legal/terms-of-service/public',
-                'selector': ''},
+                'selector': '//*[@id="content"]/div/div/div[2]/main'},
             'trustpilot': {
                 'url': 'https://legal.trustpilot.com/end-user-terms-and-conditions',
-                'selector': ''},
+                'selector': (
+                    '/html/body'
+                    + '/div[contains(@class, "content")]/main')},
             'xhamster': {
                 'url': 'https://fr.xhamster.com/info/terms',
-                'selector': ''},}
+                'selector': (
+                    '/html/body'
+                    + '/div[contains(@class, "main-wrap")]'
+                    + '/div/main/article')},}
