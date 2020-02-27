bea43b6ded7d14d958bcddc1ee4035e4280688a4
apehex <apehex@protonmail.com>
2020-02-27 08:51:01 +0100

    Remove unused import

--- homespace/cli.py
+++ homespace/cli.py
@@ -10,8 +10,6 @@ Console script.
 
 from __future__ import absolute_import, division, print_function
 
-import re
-
 import click
 
 #####################################################################
