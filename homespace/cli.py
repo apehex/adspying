df29e017da04c9a29e26eec53eaae3d968b76479
apehex <apehex@protonmail.com>
2020-02-27 08:50:24 +0100

    Document the main entry point

--- homespace/cli.py
+++ homespace/cli.py
@@ -20,7 +20,18 @@ import click
 
 @click.command()
 def main(args=None):
-    """Console script for homespace."""
+    """
+    An alternative to the scrapy shell / scrapy crawl.
+    Allows to launch several scraping jobs at once.
+
+    Parameters
+    ----------
+    args: None.
+    
+    Returns
+    -------
+    out: None.
+    """
     click.echo("Replace this message by putting your code into "
                "homespace.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
