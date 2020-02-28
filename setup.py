0831bb2d527c560d697ba75f90cb2c3da86d56e8
apehex <apehex@protonmail.com>
2020-02-28 16:02:06 +0100

    Remove the changelog from the file bundle

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [
     'beautifulsoup4',
     'geopy',
@@ -59,7 +56,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='homespace',
     name='homespace',
