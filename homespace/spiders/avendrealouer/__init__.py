257e11c881b0ae82de00ed7945a294cc6d09dd20
apehex <apehex@protonmail.com>
2020-09-09 21:49:08 +0200

    Automate version bumping for all subpackages

--- homespace/spiders/avendrealouer/__init__.py
+++ homespace/spiders/avendrealouer/__init__.py
@@ -9,10 +9,11 @@ Adapt the scraping tools to the particularities of the website avendrealouer.
 """
 
 from __future__ import division, print_function, absolute_import
+from importlib.metadata import version
 
 __author__ = 'apehex'
 __email__ = 'apehex@protonmail.com'
-__version__ = '0.1.0'
+__version__ = version(__package__)
 __title__ = 'homespace.spiders.avendrealouer'
 __description__ = 'A toolbox to scrape and visualize second hand ads.'
 __url__ = 'https://github/apehex/homespace'
