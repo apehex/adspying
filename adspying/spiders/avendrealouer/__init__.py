6cc3b2dcc7f76fb3c31c7cffcef2cdc792520d42
apehex <apehex@protonmail.com>
2020-09-12 23:42:02 +0200

    Fix the 'unknown package' error

--- adspying/spiders/avendrealouer/__init__.py
+++ adspying/spiders/avendrealouer/__init__.py
@@ -13,7 +13,7 @@ from importlib.metadata import version
 
 __author__ = 'apehex'
 __email__ = 'apehex@protonmail.com'
-__version__ = version(__package__)
+__version__ = version(__package__.split('.')[0])
 __title__ = 'adspying.spiders.avendrealouer'
 __description__ = 'A toolbox to scrape econd hand ads.'
 __url__ = 'https://github/apehex/adspying'
